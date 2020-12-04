3eb2414903a7a237ce9a9a4cae48ceb827522da0
Tetiana Serdiuk <tetianaskh@gmail.com>
2020-12-04 13:09:55 +0200

    Initial commit

---
+++ src/com/company/Task3.java
@@ -0,0 +1,51 @@
+package com.company;
+
+
+public class Task3 {
+
+    // Обязательные задачи:
+
+    // Задача 1. Максимальное из введенных чисел
+    static void exercise01() {
+
+    }
+    // Задача 2. Тест
+    static void exercise02() {
+
+    }
+    // Задача 3. Проверка даты
+    static void exercise03() {
+
+    }
+    // Задача 4. Линейное уравнение
+    static void exercise04() {
+
+    }
+    // Задача 5. Квадратное уранение
+    static void exercise05() {
+
+    }
+
+    // Дополнительные задачи:
+
+    // Задача 1. Самолет и топливо
+    static void exercise001() {
+
+    }
+    // Задача 2. Алгоритм приготовления тостов
+    static void exercise() {
+
+    }
+    // Задача 3. Жалюзи
+    static void exercise003() {
+
+    }
+    // Задача 4. Программист Вася
+    static void exercise004() {
+
+    }
+
+    public static void main(String[] args) {
+	//
+    }
+}
